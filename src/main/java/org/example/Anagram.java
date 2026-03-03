f0b3ee845cddc24b869aa1def47a0abf3e035bb2
Alfredo Barredo Garcia <FredoBG@Alfredos-MacBook-Pro.local>
2026-03-02 20:21:29 -0600

    Anagram. change char to int

--- src/main/java/org/example/Anagram.java
+++ src/main/java/org/example/Anagram.java
@@ -7,13 +7,13 @@ public class Anagram {
         if (s.length() < 1 || t.length() < 1 || s.length() != t.length())
             return false;
 
-        char[] charArr = new char[26];
+        int[] charArr = new int[26];
         for (int i = 0; i < s.length(); i++) {
             charArr[s.charAt(i) - 'a']++;
             charArr[t.charAt(i) - 'a']--;
         }
 
-        for (char c : charArr) {
+        for (int c : charArr) {
             if (c != 0)
                 return false;
         }
