28daa0bb31cf692f06c608b438fb74e06d86bdab
Alfredo Barredo <alfredobg@hotmail.com>
2026-04-28 12:17:34 -0600

    Update Palindrome

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        PrimeNums primeNums = new PrimeNums();
-        System.out.println("Is Prime: " + primeNums.isPrime(5));
-        System.out.println("First N Prime Numbers: " + primeNums.getFirstNPrimes(8));
+        //PrimeNums primeNums = new PrimeNums();
+        //System.out.println("Is Prime: " + primeNums.isPrime(5));
+        //System.out.println("First N Prime Numbers: " + primeNums.getFirstNPrimes(10));
 
         //Fibonacci fibonacci = new Fibonacci();
         //System.out.println("First N Fibonacci items: "+fibonacci.firstFibN(8));
@@ -21,8 +21,20 @@ public class Main {
         //SortTwoOrderedArrIn3rdArr sortTwoOrderedArrIn3rdArr = new SortTwoOrderedArrIn3rdArr();
         //sortTwoOrderedArrIn3rdArr.sort(new int[]{1,3,5}, new int[]{2,4,6});
 
-        //Palindrome palindrome = new Palindrome();
+        Palindrome palindrome = new Palindrome();
         //System.out.println("Is Palindrome: " + palindrome.isPalindrome("Radar"));
+        String[] tests = {
+                "1234",
+                "12321",
+                "9912321456",
+                "9912333321456",
+                "12145445499",
+                "1223213",
+                "abb"
+        };
+        for (String test : tests) {
+            System.out.println(test + " -> " + palindrome.longestPalindrome(test));
+        }
 
         //int[] nums = {3, 2, 1, 0, 4}; //{2,3,0,1,4};
         //MinNumJumpsReachIndexNMinus1 minNumJumpsReachIndexNMinus1 = new MinNumJumpsReachIndexNMinus1();
