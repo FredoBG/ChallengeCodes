8f6631081f1d9decb999bd82e745ea6b247fbbb7
Alfredo Barredo <alfredobg@hotmail.com>
2026-06-24 17:14:35 -0600

    Update Vowels

--- src/main/java/org/example/Vowels.java
+++ src/main/java/org/example/Vowels.java
@@ -2,32 +2,24 @@ package org.example;
 
 public class Vowels {
     public String reverseVowels(String s) {
+        int l=0, r=s.length()-1;
+        StringBuilder sb = new StringBuilder(s);
         String vowels = "aeiouAEIOU";
-        char[] arrS = s.toCharArray();
-        char aux;
-        int l = 0, r = s.length()-1, chg = 0;
 
-        System.out.println("Reversing vowels in: " + s);
-        while (l < r){
-            if (vowels.indexOf(s.charAt(l)) == -1){
-                l++;
-            } else {
-                chg++;
+        while (l<r){
+            if (vowels.indexOf(sb.charAt(l))>=0 && vowels.indexOf(sb.charAt(r))>=0){
+                char t = sb.charAt(l);
+                sb.setCharAt(l, sb.charAt(r));
+                sb.setCharAt(r, t);
+                l++; r--;
             }
-            if (vowels.indexOf(s.charAt(r)) == -1){
-                r--;
-            } else {
-                chg++;
-            }
-            if (chg==2){
-                aux = arrS[l];
-                arrS[l] = arrS[r];
-                arrS[r] = aux;
+            if (vowels.indexOf(sb.charAt(l))==-1){
                 l++;
+            }
+            if (vowels.indexOf(sb.charAt(r))==-1){
                 r--;
             }
-            chg=0;
         }
-        return new String(arrS);
+        return sb.toString();
     }
 }
